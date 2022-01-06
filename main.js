fdeaf93a8b9b3fa656e6115aba9ca7233bc839c7
simba155kor <simba155korea@gmail.com>
2022-01-06 22:25:51 +0900

    UPDATE things

--- main.js
+++ main.js
@@ -3,23 +3,59 @@ let url = require('url');
 let mysql = require('mysql');
 let aajs = require('./lib/aa.js');
 
+
+
 let app = http.createServer(function(request,response){
     let _url = request.url;
     let queryData = url.parse(_url, true).query;
     let pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
+
+    let pathname_split = pathname.split('/');
+
+    if(pathname_split.length===0){
       if(queryData.id === undefined){
 
       } else {
           
       }
-    } else if(pathname === '/create'){
-      topic.create(request, response);
-    } else if(pathname === '/create_process'){
-      topic.create_process(request, response);
-    } else if(pathname === '/Likestock'){
-        if(request.method == "GET") aajs.get();
-        else if(request.method == "UPDATE") aajs.update();
+    } else if(pathname_split[0] === 'member'){
+        if(pathname_split.length===1){
+            if(request.method === "GET") { console.log("1");}
+            else if(request.method === "POST") {}
+            else if(request.method === "PUT") {}
+            else if(request.method === "DELETE") {} 
+        } else{
+            if(pathname_split[1] === 'signup') { }
+            else if(pathname_split[1] === 'allmember') {}
+            else if(pathname_split[1] === 'edit') {}
+            else if(pathname_split[1] === 'login') {}
+            else if(pathname_split[1] === 'findPW') {}
+        }
+    } else if(pathname_split[0] === 'memberstock'){
+        if(request.method === "GET") {}
+        else if(request.method === "POST") {}
+        else if(request.method === "DELETE") {}
+
+    } else if(pathname_split[0] === 'news'){
+        if(request.method === "GET") {}
+        else if(request.method === "POST") {}
+        else if(request.method === "DELETE") {} 
+        
+    } else if(pathname_split[0] === 'reply'){
+        if(request.method === "GET") {}
+        else if(request.method === "POST") {}
+        else if(request.method === "PUT") {}
+        else if(request.method === "DELETE") {} 
+        
+    } else if(pathname_split[0] === 'stock'){
+        if(pathname_split.length===1){
+            if(request.method === "GET") {}
+            else if(request.method === "POST") {}
+            else if(request.method === "PUT") {}
+            else if(request.method === "DELETE") {} 
+        } else{
+            if(pathname_split[1] === 'searchAll') { }
+        }
         
     } else {
       response.writeHead(404);
