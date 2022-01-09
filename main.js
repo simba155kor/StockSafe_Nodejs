95cbaeafd30d68f091f92cc52cec1897764becba
simba155kor <simba155korea@gmail.com>
2022-01-09 15:41:13 +0900

    UPDATE things

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let http = require("http");
 let url = require("url");
 let mysql = require("mysql");
 let aajs = require("./lib/aa.js");
+let member = require("./lib/member.js");
 let db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -12,20 +13,12 @@ let db = mysql.createConnection({
 // cors error
 // 왜 요청 데이터를 못받냐
 
-
 let app = http.createServer(function (request, response) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000, // 30 days
-    "Access-Control-Allow-Headers": "*"
-    /** add other headers as per requirement */
-  };
-
   let _url = request.url;
   let queryData = url.parse(_url, true).query;
   let pathname = url.parse(_url, true).pathname;
 
+  // "/"
   let pathname_split = pathname.split("/");
   console.log(pathname_split);
   if (pathname_split.length === 1) {
@@ -33,14 +26,14 @@ let app = http.createServer(function (request, response) {
     } else {
     }
   } else if (pathname_split[1] === "member") {
-      console.log("2");
+    console.log("2");
     if (pathname_split.length === 2) {
       if (request.method === "GET") {
         console.log("1");
-        db.query(`select * from member`, function(error, data){
+        db.query(`select * from member`, function (error, data) {
           console.log(request);
           console.log(data);
-        })
+        });
       } else if (request.method === "POST") {
       } else if (request.method === "PUT") {
       } else if (request.method === "DELETE") {
@@ -50,45 +43,14 @@ let app = http.createServer(function (request, response) {
       } else if (pathname_split[2] === "allmember") {
       } else if (pathname_split[2] === "edit") {
       } else if (pathname_split[2] === "login") {
-        if(request.method === "POST"){
-          let body = [];
-          request.on('data', (chunk) => {
-            body.push(chunk);
-          }).on('end', () => {
-            body = Buffer.concat(body).toString();
-            body = JSON.parse(body);
-            // 여기서 `body`에 전체 요청 바디가 문자열로 담겨있습니다.
-            console.log(body);
-            console.log(body.id);
-            console.log(body.memberPw);
-            console.log("!!!!");
-            db.query(`
-        select member_name
-        from member
-        where id=? and member_pw=?`, [body.id, body.memberPw], function(error, data){
-          console.log(data);
-          console.log("!!");
-          if(data.length === 1){
-            console.log(data);
-          response.writeHead(200, headers);
-          response.write(data[0].member_name);
-          response.end();
-          }
-          else{
-            console.log('fail!');
-            response.writeHead(204, headers);
-            response.write("fail");
-            response.end();
-          }
-             });
-          });
-        }
-        else if(request.method === "OPTIONS"){
-          console.log('options!');
+        if (request.method === "POST") {
+          console.log("11");
+          member.login(db, request, response);
+        } else if (request.method === "OPTIONS") {
+          console.log("options!");
           response.writeHead(200, headers);
           response.end();
-        }
-        else{
+        } else {
           //console.log(request);
           let body = "";
           request.on("data", function (data) {
@@ -127,12 +89,13 @@ let app = http.createServer(function (request, response) {
     } else {
       if (pathname_split[2] === "searchAll") {
         console.log("!");
-        response.writeHead(302, {Location : `/`});
+        response.writeHead(302, { Location: `/` });
         response.end();
       }
     }
   } else {
-    response.writeHead(404, headers);
+    console.log("!>");
+    response.writeHead(404, " ");
     response.end("Not found");
   }
 });
