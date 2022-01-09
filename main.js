8e842ed921f5cc9f78ff48bb9bb5f284e7a08a14
simba155kor <simba155korea@gmail.com>
2022-01-09 21:54:48 +0900

    UPDATE things

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 let http = require("http");
 let url = require("url");
-let mysql = require("mysql");
-let aajs = require("./lib/aa.js");
+let mysql = require("./lib/mysql.js");
 let member = require("./lib/member.js");
 let likestock = require("./lib/likestock.js");
 
-let db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "122333",
-  database: "stocksafe_db",
-});
 
 // cors error
 // 왜 요청 데이터를 못받냐
@@ -39,7 +32,7 @@ let app = http.createServer(function (request, response) {
     if (pathname_split.length === 2) {
       if (request.method === "GET") {
         console.log("1");
-        db.query(`select * from member`, function (error, data) {
+        mysql.db.query(`select * from member`, function (error, data) {
           console.log(request);
           console.log(data);
         });
@@ -54,7 +47,7 @@ let app = http.createServer(function (request, response) {
       } else if (pathname_split[2] === "login") {
         if (request.method === "POST") {
           console.log("11");
-          member.login(db, request, response);
+          member.login(mysql.db, request, response);
         } else if (request.method === "OPTIONS") {
           console.log("options!");
           response.writeHead(200, headers);
@@ -96,9 +89,9 @@ let app = http.createServer(function (request, response) {
     //console.log(request);
     if (request.method === "GET") {
       console.log("!");
-      likestock.readLikeStock(db, request, response);
+      likestock.readLikeStock(mysql.db, request, response);
     } else if (request.method === "POST") {
-      likestock.createLikeStock(db, request, response);
+      likestock.createLikeStock(mysql.db, request, response);
     } else if (request.method === "DELETE") {
     } else if (request.method === "OPTIONS"){
       response.writeHead(204, headers);
